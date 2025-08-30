f44dd1c0ff3947e0b7c6c9457a0e732eb815ea78
iCrishGlich <cristhianadielm@gmail.com>
2025-08-30 08:29:32 -0600

    Describe tus cambios aquí

--- lib/players/player.dart
+++ lib/players/player.dart
@@ -1,13 +1,14 @@
 import 'dart:async';
 
+import 'package:flame/collisions.dart';
 import 'package:flame/components.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/services.dart';
+import 'package:star_wars/enemies/proyect_enemy.dart';
 import 'package:star_wars/main.dart';
 import 'package:star_wars/players/proyectail.dart';
 
-class Player extends SpriteComponent with KeyboardHandler {
-  double speed = 200;
+class Player extends SpriteComponent with KeyboardHandler, CollisionCallbacks {
+  double speed = 400;
   Vector2 velocity = Vector2.zero();
 
   @override
@@ -19,7 +20,7 @@ class Player extends SpriteComponent with KeyboardHandler {
       GalaxyWars.screenSize.y - 70,
     );
     size = Vector2(100, 100);
-
+    add(RectangleHitbox());
     return super.onLoad();
   }
 
@@ -28,10 +29,10 @@ class Player extends SpriteComponent with KeyboardHandler {
     velocity = Vector2.zero();
 
     if (keysPressed.contains(LogicalKeyboardKey.arrowLeft)) {
-      velocity.x = -200;
+      velocity.x = -400;
     }
     if (keysPressed.contains(LogicalKeyboardKey.arrowRight)) {
-      velocity.x = 200;
+      velocity.x = 400;
     }
     if (keysPressed.contains(LogicalKeyboardKey.space)) {
       shoot();
@@ -41,13 +42,22 @@ class Player extends SpriteComponent with KeyboardHandler {
 
   @override
   void update(double dt) {
-    // TODO: implement update
     super.update(dt);
     position += velocity * dt;
     position.clamp(
       Vector2(size.x / 2, position.y),
       Vector2(GalaxyWars.screenSize.x - size.x / 2, position.y),
     );
+  }
+
+  @override
+  void onCollision(Set<Vector2> intersectionPoints, PositionComponent other) {
+    super.onCollision(intersectionPoints, other);
+    if (other is ProyectailEnemy) {
+      other.removeFromParent();
+      GalaxyWars.life -= 1;
+      print("Te quedan: ${GalaxyWars.life}");
+    }
   }
 
   void shoot() {
--- lib/players/proyectail.dart
+++ lib/players/proyectail.dart
@@ -4,13 +4,14 @@ import 'package:flame/collisions.dart';
 import 'package:flame/components.dart';
 
 class Proyectail extends SpriteComponent with CollisionCallbacks {
-  final double speed = 200;
+  final double speed = 300;
 
   @override
   FutureOr<void> onLoad() async {
     sprite = await Sprite.load("proyectail.png");
     size = Vector2(30, 50);
     add(RectangleHitbox());
+    anchor = Anchor.center;
     return super.onLoad();
   }
 
